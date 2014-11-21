04054f8cc3016af498d82bc89797e60a88763928
Luke Fraser <lukefrasera@gmail.com>
2014-11-20 21:35:37 -0800

    adding new assignment directory

---
+++ assignment_04/src/convolution_freq.cc
@@ -0,0 +1,26 @@
+//"convolution_freq.cc"
+//
+//  Copyright (c) Luke Fraser 2014
+//
+//  This file is part of cs674Class.
+//
+//    cs674Class is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    cs674Class is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with cs674Class.  If not, see <http://www.gnu.org/licenses/>.
+#include "ReadImage.h"
+#include "ReadImageHeader.h"
+#include "WriteImage.h"
+#include <stdio.h>
+
+int main(int argc, char * argv[]) {
+  return 0;
+}
